31cddd4cc9242ac228f968c840ff6b325736c610
Richard Cotton <richierocks@gmail.com>
2017-08-31 14:22:57 -0400

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 library(devtools)
 
 install_github("hafen/trelliscopejs")
-install_version("rbokeh", "0.5.0")
+#install_version("rbokeh", "0.5.0")
+install_github("bokeh/rbokeh")
 install_version("ggplot2", "2.2.1")
-install_version("gapminder", "0.2.0")
-
+install_version("dplyr", "0.7.2")
